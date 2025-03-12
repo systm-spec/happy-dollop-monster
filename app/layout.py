1b0a2023a7ac3b384ab843b850af974fb67107bc
Stread <83221877+systm-spec@users.noreply.github.com>
2025-03-12 19:33:36 +0100

    Layout begonnen
    -Head mit logo
    - searchbar elemente
    - erste display elemente

--- app/layout.py
+++ app/layout.py
@@ -21,11 +21,43 @@ farbe = {
 # App layout
 app.layout = [
     html.Div(children=[
+        html.Div(className='flex-col', children=[
 
-        html.Div(className='panel', children=[
-            html.H1('Pokemon Dashboard')
+            html.Div(id='header',className='panel flex', children=[
+                html.Img(src='/assets/img/pika_tile.png'),
+                html.H1('Pokemon Dashboard')
+            ]),
+
+            html.Div(id='search-bar', className='panel flex-col', children=[
+                html.H2('Search Your Pokemon'),
+                html.Div(className='', children=[
+                    html.Button(className='', children='Search',id="search-btn"),
+                    dcc.Input(
+                        value='',
+                        id="search-input",
+                        type="search",
+                        placeholder="Suchbegriff eingeben...",
+                        debounce=True,  # Verhindert zu viele Updates bei jedem Tastenanschlag
+                        style={"width": "300px", "padding": "10px", "fontSize": "16px"}
+                    ),
+                ])
+
+            ]),
+            html.Div(id='pokemon-panel-one', className='panel flex', children=[
+                html.Div(className="thumbnail_pokemon", children=[
+                    html.Img(src='assets/img/Icons/gen_1/025-Pikachu.72.png')
+                ]),
+
+                html.Div(className='', children=[
+                    html.H2('Pikachu'),
+                    html.P('Mouse Pokemon'),
+
+                ])
+
+            ])
+
         ])
-    ], className='flex')
+    ])
 ]
 
 # Run
