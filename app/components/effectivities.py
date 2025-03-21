95a7ca2eeb08ec84f2b53306b17f1ac74981b6e6
Stread <83221877+systm-spec@users.noreply.github.com>
2025-03-21 11:39:23 +0100

    Update effectivities.py

--- app/components/effectivities.py
+++ app/components/effectivities.py
@@ -1,20 +1,47 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
 import os
 
+df = pd.read_csv('../../data/csv/pokedex.csv', sep=',')
+types_df = df[['pokedex_number',
+               'german_name',
+               'type_1',
+               "type_2",
+               'against_fire',
+               'against_bug',
+               'against_dark',
+               'against_dragon',
+               'against_electric',
+               'against_fairy',
+               'against_fight',
+               'against_flying',
+               'against_ghost',
+               'against_grass',
+               'against_ice',
+               'against_ground',
+               'against_normal',
+               'against_poison',
+               'against_psychic',
+               'against_rock',
+               'against_steel',
+               'against_water']]
 
-df = pd.read_csv('../../data/csv/pokedex.csv', sep=',', )
 print('Init...\n')
-#print(os.path.abspath('../../data/csv/pokedex.csv'))
-#print(df.shape, df.head(2))
+# print(os.path.abspath('../../data/csv/pokedex.csv'))
+# print(df.shape, df.head(2))
 
 app = Dash()
 
+
 def generate_effectivity_plot(pkmn):
-    active_mon = df[df['german_name'] == pkmn]
+    active_mon = types_df[types_df['german_name'] == pkmn]
+    fig = px.imshow(active_mon[5:], zmin=0, zmax=4)
     return html.Div([
         html.H3(f"Name: {active_mon['german_name'].values[0]}"),
-        html.P(f"Pokedex Nummer: {active_mon['pokedex_number'].values[0]}")
+        html.P(f"Pokedex Nummer: {active_mon['pokedex_number'].values[0]}"),
+
+        dcc.Graph(id='effectivity-content', figure=fig)
     ])
 
 
